0cb1f568939809c05ab287b2a7cfb7754816cf1c
Masyko <55163725+Masyko@users.noreply.github.com>
2021-10-18 20:20:53 +0300

    Update test_tic_tac_toe.py

--- test_tic_tac_toe.py
+++ test_tic_tac_toe.py
@@ -1,8 +1,11 @@
 import unittest
-from tic_tac_toe import main, check_win, take_input, draw_board
+import tic_tac_toe
 
 class InputTestCase(unittest.TestCase):
-    def test_take_input(self):
-        game = main()
-        assert a == input('ron')
-        assert b == input('dgfhghuijlok;ojihugfdgfhgjkhljk;ljhlgk')
+    def test_win_x(self):
+        board = [1, 2, 3, 'X', 'X', 'X', 'O', 'O', 9]
+        self.assertEqual(tic_tac_toe.check_win(board), 'X')
+
+    def test_win_o(self):
+        board = ['X', 'X', 'O', 'X', 'X', 'O', 7, 8, 'O']
+        self.assertEqual(tic_tac_toe.check_win(board), 'O')
